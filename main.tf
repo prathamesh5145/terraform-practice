0d50e553c26148e6e5154fc246721151508d950d
prathamesh <prathameshchavan5145gmail.com>
2024-02-14 14:59:14 +0530

    hsrhstr

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "my_security" {
     to_port = 80
   }
   egress {
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
     protocol = "-1"
     from_port = 0
     to_port = 0
