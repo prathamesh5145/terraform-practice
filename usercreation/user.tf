a62759e94b49e1ccd0ec0b7a13d620fc75cb7527
prathamesh <prathameshchavan5145gmail.com>
2024-02-14 17:35:07 +0530

    asgs

--- usercreation/user.tf
+++ usercreation/user.tf
@@ -9,3 +9,20 @@ resource "aws_s3_bucket" "practice_bucket" {
 resource "aws_iam_user" "new_user" {
   name = "akshata"
 }
+
+resource "aws_iam_policy" "iam_s3_policy" {
+  name        = "specific-bucket-access-Policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = aws_s3_bucket.practice_bucket.arn 
+      },
+    ]
+  })
+}
